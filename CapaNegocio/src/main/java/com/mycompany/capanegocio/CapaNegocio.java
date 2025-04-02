83c97e23dcc09a8b11f9ba9916182a4bfcd18fa0
RamonVH18 <ramon.valencia253272@potros.itson.edu.mx>
2025-04-02 14:41:17 -0700

    Creacion del repositorio
    Se creo el repositorio y se agregaron las 4 capas del proyecto

---
+++ CapaNegocio/src/main/java/com/mycompany/capanegocio/CapaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capanegocio;
+
+/**
+ *
+ * @author Ramon Valencia 
+ */
+public class CapaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
