babd54112ee768fd003f3ecef0081a0a40006fd8
RamonVH18 <ramon.valencia253272@potros.itson.edu.mx>
2025-04-02 19:18:42 -0700

    MiniPush
    Creacion de las entidades IngredientesProducto e Ingredientes

--- CapaDominio/src/main/java/entidades/Ingrediente.java
+++ CapaDominio/src/main/java/entidades/Ingrediente.java
@@ -5,22 +5,49 @@
 package entidades;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
- * @author rodri
+ * @author Ramon Valencia
  */
 @Entity
+@Table (name = "Ingredientes")
 public class Ingrediente implements Serializable {
 
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column (name = "Nombre_Ingrediente", nullable = false, length = 50)
+    private String nombre;
+    
+    @Column (name = "Unidad_Medida", nullable = false, length = 50, unique = true)
+    private String unidadMedida;
+    
+    @Column (name = "Cantidad_Actual", nullable = false)
+    private Double cantidadActual; 
+
+    public Ingrediente() {
+    }
+
+    public Ingrediente(Long id, String nombre, String unidadMedida, Double cantidadActual) {
+        this.id = id;
+        this.nombre = nombre;
+        this.unidadMedida = unidadMedida;
+        this.cantidadActual = cantidadActual;
+    }
+
+    public Ingrediente(String nombre, String unidadMedida, Double cantidadActual) {
+        this.nombre = nombre;
+        this.unidadMedida = unidadMedida;
+        this.cantidadActual = cantidadActual;
+    }
 
     public Long getId() {
         return id;
@@ -30,29 +57,37 @@ public class Ingrediente implements Serializable {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public String getNombre() {
+        return nombre;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Ingrediente)) {
-            return false;
-        }
-        Ingrediente other = (Ingrediente) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+
+    public Double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(Double cantidadActual) {
+        this.cantidadActual = cantidadActual;
     }
 
     @Override
     public String toString() {
-        return "entidades.Ingrediente[ id=" + id + " ]";
+        return "Ingrediente{" + 
+                "id=" + id + 
+                ", nombre=" + nombre + 
+                ", unidadMedida=" + unidadMedida + 
+                ", cantidadActual=" + cantidadActual + 
+                '}';
     }
-    
 }
--- CapaDominio/src/main/java/entidades/IngredientesProducto.java
+++ CapaDominio/src/main/java/entidades/IngredientesProducto.java
@@ -12,12 +12,12 @@ import javax.persistence.Id;
 
 /**
  *
- * @author rodri
+ * @author Ramon Valencia
  */
 @Entity
 public class IngredientesProducto implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,31 +28,6 @@ public class IngredientesProducto implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof IngredientesProducto)) {
-            return false;
-        }
-        IngredientesProducto other = (IngredientesProducto) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entidades.IngredientesProducto[ id=" + id + " ]";
     }
     
 }
